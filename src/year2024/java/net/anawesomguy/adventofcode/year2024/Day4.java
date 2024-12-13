b0060459e8c905bae9b899a66a0bcc140e4f7083
AnAwesomGuy <79700396+AnAwesomGuy@users.noreply.github.com>
2024-12-13 08:38:15 +0800

    part 2 (honestly what i barely understand my own code)

--- src/year2024/java/net/anawesomguy/adventofcode/year2024/Day4.java
+++ src/year2024/java/net/anawesomguy/adventofcode/year2024/Day4.java
@@ -4,7 +4,6 @@ import it.unimi.dsi.fastutil.ints.IntArrayList;
 import it.unimi.dsi.fastutil.ints.IntList;
 import net.anawesomguy.adventofcode.InvalidInputException;
 import net.anawesomguy.adventofcode.Puzzle.LineStreamed;
-import org.apache.commons.lang3.mutable.MutableInt;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
@@ -13,21 +12,26 @@ import java.util.stream.Stream;
 
 public final class Day4 implements LineStreamed {
     private XMASLetter[][] crossword; // 2d array of the input
-    private final List<IntList> xIndices = new ArrayList<>();
+    private final List<IntList> xIndices = new ArrayList<>(140);
+    private final List<IntList> aIndices = new ArrayList<>(140);
 
     @Override
     public void input(Stream<String> stream) throws InvalidInputException {
-        if ((this.crossword = stream.map(s -> {
-                int length = s.length();
-                XMASLetter[] letters = new XMASLetter[length];
-                IntList xIndices = new IntArrayList();
-                for (int i = 0; i < length; i++)
-                    if ((letters[i] = XMASLetter.getFromChar(s.charAt(i))) == XMASLetter.X)
-                        xIndices.add(i);
-                Day4.this.xIndices.add(xIndices);
-                return letters;
-            }).toArray(XMASLetter[][]::new)).length != xIndices.size())
-            throw new InvalidInputException();
+        this.crossword = stream.map(s -> {
+            int length = s.length();
+            XMASLetter[] letters = new XMASLetter[length];
+            IntList xIndices = new IntArrayList(), aIndices = new IntArrayList();
+            for (int i = 0; i < length; i++) {
+                XMASLetter letter = letters[i] = XMASLetter.getFromChar(s.charAt(i));
+                if (letter == XMASLetter.X)
+                    xIndices.add(i);
+                else if (letter == XMASLetter.A)
+                    aIndices.add(i);
+            }
+            Day4.this.xIndices.add(xIndices);
+            Day4.this.aIndices.add(aIndices);
+            return letters;
+        }).toArray(XMASLetter[][]::new);
     }
 
     @Override
@@ -36,6 +40,7 @@ public final class Day4 implements LineStreamed {
         XMASLetter[][] crossword = this.crossword;
         List<IntList> xIndices = this.xIndices;
         int size = xIndices.size();
+        Direction[] directions = Direction.VALUES;
         for (int y = 0; y < size; y++) {
             IntList ints = xIndices.get(y);
             XMASLetter[] letters = crossword[y];
@@ -44,11 +49,11 @@ public final class Day4 implements LineStreamed {
                 if (letter == null)
                     continue;
                 assert letter == XMASLetter.X;
-                for (Direction dir : Direction.VALUES) {
+                for (Direction dir : directions) {
                     XMASLetter current = letter;
-                    MutableInt cursor = new MutableInt(1);
+                    int scale = 0;
                     while ((current = current.next()) != null &&
-                           current == dir.getFromOffset(crossword, index, y, cursor)) {
+                           current == dir.getFromOffset(crossword, index, y, ++scale)) {
                         if (current == XMASLetter.S) {
                             result++;
                             break;
@@ -62,7 +67,35 @@ public final class Day4 implements LineStreamed {
 
     @Override
     public int solvePart2() {
-        return 0;
+        int result = 0;
+        XMASLetter[][] crossword = this.crossword;
+        List<IntList> aIndices = this.aIndices;
+        int size = aIndices.size();
+        Direction upLeftDir = Direction.UP_LEFT, downLeftDir = Direction.DOWN_LEFT,
+                  upRightDir = Direction.UP_RIGHT, downRightDir = Direction.DOWN_RIGHT;
+        XMASLetter m = XMASLetter.M, s = XMASLetter.S;
+        for (int y = 0; y < size; y++) {
+            IntList ints = aIndices.get(y);
+            XMASLetter[] letters = crossword[y];
+            for (int index : ints) {
+                XMASLetter letter = letters[index];
+                if (letter == null)
+                    continue;
+                assert letter == XMASLetter.A;
+                XMASLetter upLeft = upLeftDir.getFromOffset(crossword, index, y),
+                           upRight = upRightDir.getFromOffset(crossword, index, y),
+                           downLeft = downLeftDir.getFromOffset(crossword, index, y),
+                           downRight = downRightDir.getFromOffset(crossword, index, y);
+                // monstrosity
+                if (upLeft == downLeft) {
+                    if (downRight == upRight && ((upLeft == m && downRight == s) || (upLeft == s && downRight == m)))
+                        result++;
+                } else if (upRight == upLeft)
+                    if (downLeft == downRight && ((upRight == m && downLeft == s) || (upRight == s && downLeft == m)))
+                        result++;
+            }
+        }
+        return result;
     }
 
     public enum XMASLetter {
@@ -107,13 +140,16 @@ public final class Day4 implements LineStreamed {
             this.yOffset = yOffset;
         }
 
-        public <T> T getFromOffset(T[][] arr2d, int x, int y, MutableInt cursor) {
-            int cur = cursor.getAndIncrement();
-            int newY = y - (yOffset * cur);
+        public <T> T getFromOffset(T[][] arr2d, int x, int y) {
+            return getFromOffset(arr2d, x, y, 1);
+        }
+
+        public <T> T getFromOffset(T[][] arr2d, int x, int y, int scale) {
+            int newY = y - (yOffset * scale);
             if (newY < 0 || newY >= arr2d.length)
                 return null;
             T[] arr = arr2d[newY];
-            int newX = x + (xOffset * cur);
+            int newX = x + (xOffset * scale);
             if (newX < 0 || newX >= arr.length)
                 return null;
             return arr[newX];
