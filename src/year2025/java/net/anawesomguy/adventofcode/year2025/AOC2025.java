274b81af86af3ad68587bb72aa710b81f87fd040
AnAwesomGuy <79700396+AnAwesomGuy@users.noreply.github.com>
2025-12-13 13:53:10 +0800

    day 10 (pt1 works but pt2 is super slow)

--- src/year2025/java/net/anawesomguy/adventofcode/year2025/AOC2025.java
+++ src/year2025/java/net/anawesomguy/adventofcode/year2025/AOC2025.java
@@ -9,7 +9,7 @@ public final class AOC2025 implements AdventOfCode.Annotated {
 
     public static void main(String... args) {
         // AdventOfCode.main(args);
-        // AdventOfCode.solveAndSubmit(Day10.class, true);
+        // AdventOfCode.solveAndSubmit(Day10.class, false);
         // AdventOfCode.getInputAndSolve(Day10.class);
         // AdventOfCode.solvePuzzleWithInput(Day9.class, """
         //     7,1
@@ -22,7 +22,7 @@ public final class AOC2025 implements AdventOfCode.Annotated {
         //     7,3
         //     """);
         AdventOfCode.solvePuzzleWithInput(Day10.class, """
-            [.....####.] (2,4,8) (0,1,2,3,4,7,8,9) (0,2,3,8,9) (0,1,2,4,6,7,8) (2,4,6,9) (0,3,5,6,8,9) (0,1,3,6,8) (0,1,2,6,7,8) (0,2,3,4,5,6,7,8) {95,57,91,68,66,24,71,53,109,62}
+            [##...###.#] (0,1,5,6,7,8,9) (4,5) (1,2,3,5,6) (0,3) (8,9) (0,3,5,6,7,8,9) (0,1,4,6,7,9) (1,2) (5,8) {51,38,12,25,9,52,42,42,58,49}
             """);
     }
 }
--- src/year2025/java/net/anawesomguy/adventofcode/year2025/Day10.java
+++ src/year2025/java/net/anawesomguy/adventofcode/year2025/Day10.java
@@ -4,6 +4,7 @@ import net.anawesomguy.adventofcode.AdventDay;
 import net.anawesomguy.adventofcode.InvalidInputException;
 import net.anawesomguy.adventofcode.Puzzle;
 
+import java.util.Arrays;
 import java.util.stream.Stream;
 
 @AdventDay(day = 10)
@@ -21,20 +22,23 @@ public final class Day10 implements Puzzle.LineStreamed {
         for (var machine : machines) {
             long fewest = Machine.fewestPresses(machine.lights(), machine.buttons());
             result += fewest;
-            System.out.println(fewest);
         }
         return result;
     }
 
     @Override
     public long solvePart2() {
-        return 0;
+        return Arrays.stream(machines)
+                     .parallel()
+                     .mapToLong(Machine::fewestPresses)
+                     .peek(System.out::println)
+                     .sum();
     }
 
     // lights is a "bit set" from left to right, same with buttons
     // this allows us to xor a button with the lights to get the result easily
     // 0x8000 is a 16-bit integer with the top bit set
-    public record Machine(char lights, char[] buttons, int[] joltages) {
+    public record Machine(char lights, char[] buttons, short[] joltages) {
         public static char lightsFrom(String s) {
             int i = s.length() - 1;
             char lights = 0;
@@ -54,8 +58,13 @@ public final class Day10 implements Puzzle.LineStreamed {
             return buttons;
         }
 
-        public static int[] joltagesFrom(String s) {
-            return Puzzle.mapToInts(s.substring(1, s.length() - 1).split(","));
+        public static short[] joltagesFrom(String s) {
+            String[] split = s.substring(1, s.length() - 1).split(",");
+            int n = split.length;
+            short[] i = new short[n];
+            while (n-- > 0)
+                i[n] = (short)Integer.parseInt(split[n]);
+            return i;
         }
 
         public static Machine fromString(String s) {
@@ -77,18 +86,53 @@ public final class Day10 implements Puzzle.LineStreamed {
         }
 
         private static long presses(char lights, char[] buttons, int start, long depth) {
-            long newDepth = depth;
             long largest = Long.MIN_VALUE;
             for (int i = start, len = buttons.length; i < len; i++) {
                 char button = buttons[i];
                 if (lights == button)
                     return depth;
-                if (newDepth > 1 && i + 1 < len) {
-                    long press = presses((char)(lights ^ button), buttons, i + 1, newDepth - 1);
-                    if (press > largest) {
-                        newDepth = depth - press + 1;
+                if (depth > 1 && i + 1 < len) {
+                    long press = presses((char)(lights ^ button), buttons, i + 1, depth - 1);
+                    if (press > largest)
                         largest = press;
-                    }
+                }
+            }
+            return largest;
+        }
+
+        public long fewestPresses() {
+            var shorts = new short[joltages.length];
+            long depth = 4;
+            while (true) {
+                long press = presses(lights, buttons, depth += 6, joltages, shorts);
+                if (press != Long.MIN_VALUE)
+                    return depth - press + 1;
+            }
+        }
+
+        private static long presses(char lights, char[] buttons, long depth, short[] joltagesTarget, short[] joltages) {
+            long largest = Long.MIN_VALUE;
+            long newDepth = depth;
+            outer:
+            for (char button : buttons) {
+                boolean valid = true;
+                short[] joltagesCopy = Arrays.copyOf(joltages, joltagesTarget.length);
+                {
+                    int i = 0;
+                    char buttonC = button;
+                    do {
+                        short targetJoltage = joltagesTarget[i], joltage = (joltagesCopy[i++] += (short)(buttonC >>> 15 & 1)); // 0 or 1
+                        if (joltage > targetJoltage)
+                            continue outer;
+                        valid &= joltage == targetJoltage;
+                    } while ((buttonC <<= 1) != 0);
+                }
+                if (valid)
+                    return depth;
+                if (newDepth > 1) {
+                    long press = presses((char)(lights ^ button), buttons, newDepth - 1, joltagesTarget, joltagesCopy);
+                    if (press > largest && (newDepth = depth - (largest = press)) == 1)
+                        return press;
                 }
             }
             return largest;
